c9ea2203c5d9cc4055b8f6db93e795d8d06cc56b
sriman577 <46971497+sriman577@users.noreply.github.com>
2020-04-30 12:03:09 +0530

    Update Find_the_kth_Light.py

--- Find_the_kth_Light.py
+++ Find_the_kth_Light.py
@@ -1,18 +1,24 @@
+#function to count the number 
+# of numbers in range 1-num that 
+# are divisible by given  
+# prime numbers in switch[]
+#n is length of switch[]
 def findrank(num,n):
-    rank=0
-    global switch
+    inclusion=0
+    exclusion=0
+    global switch  #made switch(On state switches) global
     for i in range(1,1<<n):
         count=0
         product=1
         for j in range(n):
-            if(i & 1<<j):
+            if(i & (1<<j)):
                 count+=1
                 product=product*switch[j]
         if count%2==1:
-            rank+=num//product
+            inclusion+=num//product
         else:
-            rank-=num//product
-    return rank
+            exclusion+=num//product
+    return inclusion-exclusion
 
 t=int(input())
 for itr in range(t):
@@ -25,9 +31,11 @@ for itr in range(t):
     low=k
     res=-1
     n=len(switch)
-    high=k*switch[0]
+    high=switch[0]*k
+    #do binary search from k to min(switch)*k
     while(low<=high):
         mid=low+(high-low)//2
+        # find the rank of mid. no of numbers that are divisible by any one prime in switch list
         if findrank(mid,n)>=k:
             res=mid
             high=mid-1
